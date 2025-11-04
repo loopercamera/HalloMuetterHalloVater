0fc9ebe26d8f343d8ffdeb9f7be3067b4ae506f8
JanisKramer <janis.kramer@students.fhnw.ch>
2025-11-04 14:07:48 +0100

    Update_files_Vorlage_Wuest

--- HalloMuetterHalloVater/capacitor.config.ts
+++ HalloMuetterHalloVater/capacitor.config.ts
@@ -2,8 +2,14 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'HalloMuetterHalloVater',
+  appName: 'testapp_2025',
   webDir: 'www'
+  android: {
+  allowMixedContent: true,
+  },
+  server: {
+  cleartext: true,
+  }
 };
 
 export default config;
